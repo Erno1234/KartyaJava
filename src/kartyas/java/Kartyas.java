d1dbe38b673c62932decfb4f6bab494aff72aa42
Erno1234 <palocierno12>
2023-09-12 21:28:22 +0200

    Ütközések javítása, hibás case 2

--- src/kartyas/java/Kartyas.java
+++ src/kartyas/java/Kartyas.java
@@ -10,15 +10,16 @@ public class Kartyas {
     public static void main(String[] args) {
         Feltolt();
         Kirak();
-        Kever(Melyik());
-        EzVolt();
-      
+        int oszlop = Melyik();
+        Kever(oszlop);
+
+        //EzVolt();
     }
 
     private static void Kirak() {
         for (int i = 0; i < pakli.length; i++) {
 
-            System.out.printf("%-8s",pakli[i]);
+            System.out.printf("%-8s", pakli[i]);
             if (i % 3 == 0) {
                 System.out.println("");
             }
@@ -28,69 +29,46 @@ public class Kartyas {
     private static int Melyik() {
         boolean jo;
         int oszlop;
-        do {            
+        do {
             System.out.println("Oszlop 1-3: ");
-<<<<<<< HEAD
-            int oszlop = myObj.nextInt();
-            jo = oszlop >=1 && oszlop <=3;
-=======
             oszlop = myObj.nextInt();
-            jo = oszlop >=1 && oszlop<=3;
->>>>>>> d6f7e97e4fe139c1b7f67962e1789bd8078b8b28
+            jo = oszlop >= 1 && oszlop <= 3;
         } while (!jo);
         return oszlop;
     }
 
-<<<<<<< HEAD
-    private static void Kever() {
-<<<<<<< HEAD
-        for (int i = 0; i <= 7; i++) {
-            switch (i) {
-                case 1:
-                    i = (20 - (i - 1) * 3);
-                case 2:
-                    // [i+7] = (19 - (i - 1) * 3);
-                case 3:
-                    // [i+7] = (21 - (i - 1) * 3);
-                    break;
-                default:
-            }
-=======
-      
-            for(int i=1; i<=7; i++){
-                switch (2){
-                    case 1:
-                        //[i] = 19-(i-1)*3;
-                        break;
-                    case 2:
-                        //[i+7] = 20-(i-1)*3;
-                        break;
-                    case 3:
-                        //i+14 =21-(i-1)*3;
-                        break;
-                    default:
-                        
-                }
-                
-         
-                
-=======
     private static void Kever(int oszlop) {
+        int j;
+        int csere;
         switch (oszlop) {
             case 1:
-                //Arnold
-                break;
-            case 2:
-                //Erno
-                break;
-            case 3:
-                //Jani
-                break;
-                
 
->>>>>>> 0bca16b3ce3ba22aea40de2f10756f95640db899
->>>>>>> d6f7e97e4fe139c1b7f67962e1789bd8078b8b28
+            case 2:
+                for (int i = 1; i <= 7; i++) {
+                    j = 19 - (i - 1) * 3;
+                    csere = i;
+                    pakli[i] = pakli[j];
+                    pakli[j] = pakli[csere];
+                }
+                for (int i = 1; i <= 7; i++) {
+                    j = 20 - (i - 1) * 3;
+                    csere = i + 7;
+                    pakli[i + 7] = pakli[j];
+                    pakli[j] = pakli[csere];
+                }
+                for (int i = 1; i <= 7; i++) {
+                    j = 21 - (i - 1) * 3;
+                    csere = i + 14;
+                    pakli[i + 14] = pakli[j];
+                    pakli[j] = pakli[csere];
+                }
+
+            case 3:
+                
+                break;
+            default:
         }
+        Kirak();
     }
 
     private static void EzVolt() {
@@ -100,11 +78,11 @@ public class Kartyas {
     private static void Feltolt() {
         String[] szinek = {"P", "T", "Z", "M"};
         String[] ertekek = {"Ász", "Kir", "Fel", ",X", "IX", "VIII"};
-        
-        int i =1;
-        for (String szin : szinek){
+
+        int i = 1;
+        for (String szin : szinek) {
             for (int j = 0; i < 22 && j < ertekek.length; j++) {
-                pakli[i++]= szin + "_" + ertekek[j]+" ";
+                pakli[i++] = szin + "_" + ertekek[j] + " ";
             }
         }
     }
